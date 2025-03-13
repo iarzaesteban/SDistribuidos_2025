d0653fca0d16bcbbb9f42f10de2f13815d3c89dc
Esteban <iarzaesteban94>
2025-03-12 23:37:41 -0300

    Aplicamos un cambio para probar las actions

--- Hit1/client/client_tcp.py
+++ Hit1/client/client_tcp.py
@@ -7,7 +7,7 @@ def start_client():
         client_socket.connect((SERVER_HOST, SERVER_PORT))
         client_socket.sendall("Hola Servidor B!".encode())
         response = client_socket.recv(1024).decode()
-        print(f"Respuesta del servidor: {response}")
+        print(f"Respuesta del servidor B: {response}")
 
 if __name__ == "__main__":
     start_client()
